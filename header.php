2418c8658a4edee1ef62dbb0adcd46ce8f194671
DxProline <jendad185@gmail.com>
2025-04-13 11:14:03 +0200

    Zaměnění projektové stuktury, Vyplněné přihlášení, plně funkční hashe a ukládání do databáze

---
+++ header.php
@@ -0,0 +1,21 @@
+<nav class="navbar">
+    <div class="logo">
+        <a href="index.php"><img src="logo.png" alt="Logo"></a>
+    </div>
+    <div class="search-container">
+        <img src="lupa.png" alt="Search Icon" class="search-icon">
+        <input type="text" placeholder="Search..." class="search-bar">
+    </div>
+    <ul class="menu">
+        <li><a id="loginLink" href="login.php"><?= isset($_SESSION['user_email'])? $_SESSION['user_email']: "Přihlásit se" ?> </a></li>
+        <li>
+            <a href="shopingCart.php">
+                <img src="menu.png" alt="Menu Icon" class="menu-icon">
+            </a>
+        </li>
+        <li><a href="menu.php">Menu</a></li>
+        <li><a href="produkty.php">Produkty</a></li>
+        <li><a href="#">Služby</a></li>
+        <li><a href="kontakt.php">Kontakt</a></li>
+    </ul>
+</nav>
---
+++ komplexniForm.js
@@ -0,0 +1,39 @@
+window.onload = function() {
+    loadCart();
+};
+function validateForm(event) {
+
+    event.preventDefault();
+    const nameInput = document.getElementById('name');
+    const emailInput = document.getElementById('email');
+    const phoneInput = document.getElementById('phone');
+    let isValid = true;
+
+    
+    // Reset warnings
+    const warnings = document.querySelectorAll('.warning');
+    warnings.forEach(warning => warning.style.display = 'none');
+
+    /*neztrácí se uživatelem vyplněné hodnoty */
+    if (!emailInput.value.includes('@')) {
+        document.getElementById('email-warning').style.display = 'block';
+        emailInput.style.borderColor = 'red';
+        isValid = false;
+    } else {
+        emailInput.style.borderColor = '';
+    }
+
+    // Validate phone number
+    if (!/^\d{9}$/.test(phoneInput.value)) {
+        document.getElementById('phone-warning').style.display = 'block';
+        phoneInput.style.borderColor = 'red';
+        isValid = false;
+    } else {
+        phoneInput.style.borderColor = '';
+    }
+
+    if (isValid) {
+        alert('Formulář byl úspěšně odeslán');
+        document.getElementById('contact-form').reset();
+    }
+}
